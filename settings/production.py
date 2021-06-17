860a701907facc1da32ccfe683fb4ef8764420eb
victorstarkman <victorstarkman@gmail.com>
2021-06-17 19:32:01 -0300

    files migration

--- settings/production.py
+++ settings/production.py
@@ -8,8 +8,12 @@ ALLOWED_HOSTS = ['djangoblogdeve.herokuapp.com' ]
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.posgresql_psycopg2',
+        'NAME': 'd1la88lq6diov8',
+        'USER':'chvikglmmnrcxu',
+        'PASSWORD':'a35bb54b2a5bd0ca24307463ecb05796bc41c3c13cc3b84a9da2cafac9f171a1',
+        'HOST':'ec2-54-163-97-228.compute-1.amazonaws.com',
+        'PORT':5432,
     }
 }
 
